91ba92791434d3c8c4681fe03efe140d00b08769
Ajolore Emmanuel Babatunde <emmanuelajolore@gmail.com>
2025-09-16 07:05:28 +0100

    Setting up the plugin details and activating in on my wp-admin dashboard.

---
+++ app/public/wp-content/plugins/domain-searcher/domain-searcher.php
@@ -0,0 +1,15 @@
+<?php
+/** 
+ * Plugin Name: EmmrizTech Domain Searcher
+ * Plugin URI: https://emmriztech.com.ng
+ * Author: EmmrizTech
+ * Description: A plugin to search for available domain names for renwal and registration of UG domains. 
+ * Author URI: https://emmriztech.com.ng
+ * Version: 1.0
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: emmriztech-domain-searcher
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) or die( 'Unauthorized Access!' );
